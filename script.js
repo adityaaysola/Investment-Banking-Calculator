c38a8d01731f42372ffa1e4e9b7188ace6d5e26e
Aditya Aysola <adiays06@gmail.com>
2025-02-26 16:10:31 -0500

    Trying to do formula button

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-alert("hi how are you doing")
 console.log("script loaded");
+
 document.addEventListener("DOMContentLoaded", function() {
     const button = document.getElementById("formulaButton") 
     button.addEventListener("click", clicked);
-    function clicked() {
-        alert("hi")
+    function clicked(s) {
+        const lol = document.getElementById("formulaClass").style.visibility == "hidden"; 
+        if(lol == "hidden") {
+            lol = "visible"; 
+        }
+        else {
+            lol = "hidden"; 
+        }
     }
 });
