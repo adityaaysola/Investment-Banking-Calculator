0d5b04b505594b1bd89fd2fea3bac87c5e9ebf77
Aditya Aysola <adiays06@gmail.com>
2025-02-26 22:24:36 -0500

    added inputs

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
 document.getElementById("formulaClass").style.visibility = "hidden"; 
 
 document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("formulaButton") 
-    button.addEventListener("click", clicked);
-    function clicked() {
+    const formulaButton = document.getElementById("formulaButton"); 
+    const calculateButton = document.getElementById("calculateButton"); 
+    formulaButton.addEventListener("click", formulaClicked);
+    calculateButton.addEventListener("click", calculateClicked); 
+    
+    function formulaClicked() {
         const formulaButton = document.getElementById("formulaClass");
         if(formulaButton.style.visibility == "hidden") {
             document.getElementById("formulaClass").style.visibility = "visible"; 
@@ -12,4 +16,22 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("formulaClass").style.visibility = "hidden"; 
         }
     }
+
+    function calculateClicked() {
+        console.log("pp")
+        const i1 = document.getElementById("i1"); 
+        const i2 = document.getElementById("i2"); 
+        const i3 = document.getElementById("i3"); 
+        const i4 = document.getElementById("i4"); 
+        let v1 = i1.value
+        let v2 = i2.value
+        let v3 = i3.value
+        let v4 = i4.value
+        i1.value = ""; 
+        i2.value = ""; 
+        i3.value = ""; 
+        i4.value = ""; 
+    }
 });
+
+
