1de171f27b98d1d48b0ab6695ce039eb96825a19
Aditya Aysola <adiays06@gmail.com>
2025-02-28 16:07:49 -0500

    fixing clear/calculate logic

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const formulaButton = document.getElementById("formulaButton"); 
     const calculateButton = document.getElementById("calculateButton"); 
+    const clearButton = document.getElementById("clearButton"); 
+    let clearStatus = 0; // 0 means it does not need to be cleared, 1 means it needs to be cleared
     formulaButton.addEventListener("click", formulaClicked);
     calculateButton.addEventListener("click", calculateClicked); 
-    
+    clearButton.addEventListener("click", clearButtonClicked); 
+
     function formulaClicked() {
         const formulaElement = document.getElementById("formulaClass");
     
@@ -16,24 +19,38 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function calculateClicked() {
-        const i1 = document.getElementById("i1"); 
-        const i2 = document.getElementById("i2"); 
-        const i3 = document.getElementById("i3"); 
-        const i4 = document.getElementById("i4"); 
-        const p = document.getElementById("i1"); 
-        const n = document.getElementById("i2"); 
-        const r = document.getElementById("i3"); 
-        const t = document.getElementById("i4"); 
-        let v1 = i1.value
-        let v2 = i2.value
-        let v3 = i3.value
-        let v4 = i4.value
-        i1.value = ""; 
-        i2.value = ""; 
-        i3.value = ""; 
-        i4.value = ""; 
-        let A = p*(Math.pow(1+r/n, n*t));
-        
+        if(clearStatus === 0) {
+            alert("No data, please enter values!"); 
+        }
+        else {
+            clearStatus = 1; 
+            const i1 = document.getElementById("i1"); 
+            const i2 = document.getElementById("i2"); 
+            const i3 = document.getElementById("i3"); 
+            const i4 = document.getElementById("i4"); 
+            const display = document.getElementById("answerLabel"); 
+            let p = i1.value
+            let n = i2.value
+            let r = i3.value
+            let t = i4.value
+            let A = p*(Math.pow(1+r/n, n*t));
+            display.textContent = display.textContent.concat("your final amount after compounded interest is: ", A);     
+        }
+    }
+
+    function clearButtonClicked() {
+        if(clearStatus === 1 || i1.value != "" || i2.value != "" || i3.value != "" || i4.value != "") {
+            i1.value = ""; 
+            i2.value = ""; 
+            i3.value = ""; 
+            i4.value = ""; 
+            display.textContent = " ";   
+            clearStatus = 0; 
+        }
+        else {
+            alert("Already cleared! Enter values"); 
+        }
+
     }
 });
 
