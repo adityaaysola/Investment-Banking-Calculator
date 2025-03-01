1cee6b4640358a2c5be65a2bd441aa8f9debe646
Aditya Aysola <aa1952@g.rit.edu>
2025-03-01 11:26:06 -0500

    lol

--- script.js
+++ script.js
@@ -35,3 +35,10 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeElements = document.querySelectorAll(".fade-in");
+
+    fadeElements.forEach((el) => {
+        el.classList.add("appear");
+    });
+});
