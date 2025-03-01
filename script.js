fc4d06d8c8fb7c5daaf9d4528119fa61c72f7508
Aditya Aysola <adiays06@gmail.com>
2025-03-01 11:44:19 -0500

    almost finalized

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const formulaButton = document.getElementById("formulaButton"); 
     const calculateButton = document.getElementById("calculateButton"); 
-    const clearButton = document.getElementById("clearButton"); 
-    let clearStatus = 0; // 0 means it does not need to be cleared, 1 means it needs to be cleared
+    const clearButton = document.getElementById("clearButton");
+    const display = document.getElementById("answerLabel");  
+    let alreadyCalculated = 0; 
     formulaButton.addEventListener("click", formulaClicked);
     calculateButton.addEventListener("click", calculateClicked); 
     clearButton.addEventListener("click", clearButtonClicked); 
@@ -18,47 +19,46 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
+    function detection() {
+        if(i1.value != "" && i2.value != "" && i3.value != "" && i4.value != "") {
+            return 1; 
+        }
+        else if (i1.value != "" || i2.value != "" || i3.value != "" || i4.value != ""){
+            return 2; 
+        }
+    }
+
     function calculateClicked() {
-        if(clearStatus === 0) {
-            alert("No data, please enter values!"); 
+        let value = detection(); 
+        if(alreadyCalculated === 1) {
+            alert("Already calculated! Please enter different values!"); 
         }
         else {
-            clearStatus = 1; 
-            const i1 = document.getElementById("i1"); 
-            const i2 = document.getElementById("i2"); 
-            const i3 = document.getElementById("i3"); 
-            const i4 = document.getElementById("i4"); 
-            const display = document.getElementById("answerLabel"); 
-            let p = i1.value
-            let n = i2.value
-            let r = i3.value
-            let t = i4.value
-            let A = p*(Math.pow(1+r/n, n*t));
-            display.textContent = display.textContent.concat("your final amount after compounded interest is: ", A);     
+            if(value === 1) {
+                alreadyCalculated = 1;
+                const i1 = document.getElementById("i1"); 
+                const i2 = document.getElementById("i2"); 
+                const i3 = document.getElementById("i3"); 
+                const i4 = document.getElementById("i4"); 
+                let p = i1.value
+                let n = i2.value
+                let r = i3.value
+                let t = i4.value
+                let A = p*(Math.pow(1+r/n, n*t));
+                display.textContent = display.textContent.concat("Your final amount after compounded interest is: ", A);     
+            }
+            else {
+                alert("Not enough data, please enter all values!"); 
+            }
         }
     }
 
     function clearButtonClicked() {
-        if(clearStatus === 1 || i1.value != "" || i2.value != "" || i3.value != "" || i4.value != "") {
+            alreadyCalculated = 0; 
             i1.value = ""; 
             i2.value = ""; 
             i3.value = ""; 
             i4.value = ""; 
             display.textContent = " ";   
-            clearStatus = 0; 
-        }
-        else {
-            alert("Already cleared! Enter values"); 
-        }
-
     }
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const fadeElements = document.querySelectorAll(".fade-in");
-
-    fadeElements.forEach((el) => {
-        el.classList.add("appear");
-    });
 });
